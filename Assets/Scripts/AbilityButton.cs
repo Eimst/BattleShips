38e584d1ba482778a89da04381346b156adca3e9
n1k1ta28 <niki.kisia@gmail.com>
2024-04-28 21:13:01 +0300

    Added Keybinds for abilities

--- Assets/Scripts/AbilityButton.cs
+++ Assets/Scripts/AbilityButton.cs
@@ -12,7 +12,15 @@ public class AbilityButton : MonoBehaviour
     {
         myButton.onClick.AddListener(CallShootingManagerSetAbility);
     }
-
+    private void Update()
+    {
+        if (Input.GetKeyDown((KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("AbilityKey1"))) && abilityNumber == 1) // 3x3
+            myButton.onClick.Invoke();
+        else if (Input.GetKeyDown((KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("AbilityKey2"))) && abilityNumber == 2) // ver/hor
+            myButton.onClick.Invoke();
+        else if (Input.GetKeyDown((KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("AbilityKey3"))) && abilityNumber == 3) // sonar
+            myButton.onClick.Invoke();
+    }
     private void CallShootingManagerSetAbility()
     {
         GameObject.FindObjectOfType<ShootingManager>().SetAbility(abilityNumber);
