6c123ce22691ed9b8c8a1d8c067ed5860cf7e750
Domas1211 <domasunikas@gmail.com>
2024-05-05 22:45:33 +0300

    Icon tooltip fix
    fixing

--- Assets/Scripts/PowerIconHover.cs
+++ Assets/Scripts/PowerIconHover.cs
@@ -1,29 +1,51 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections; // Needed to use coroutines
 
 public class PowerIconHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject tooltipText; // Reference to the tooltip text
+    public float tooltipDuration = 1.0f; // Time before the tooltip disappears
 
-    public string tooltipMessage; // The message to display when hovering
+    private Coroutine hideCoroutine; // Coroutine to handle hiding the tooltip
 
-    // When the mouse enters the object
+    // Called when the mouse enters the object
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (tooltipText != null)
         {
+            if (hideCoroutine != null)
+            {
+                StopCoroutine(hideCoroutine); // Stop existing coroutine if any
+            }
+
             tooltipText.SetActive(true); // Show the tooltip
-            tooltipText.GetComponent<Text>().text = tooltipMessage; // Set the message
+
+
+            // Start a coroutine to hide the tooltip after the specified duration
+            hideCoroutine = StartCoroutine(HideTooltipAfterDelay());
         }
     }
 
-    // When the mouse exits the object
+    // Called when the mouse exits the object
     public void OnPointerExit(PointerEventData eventData)
     {
         if (tooltipText != null)
         {
+            if (hideCoroutine != null)
+            {
+                StopCoroutine(hideCoroutine); // Stop the coroutine to prevent premature hiding
+            }
+
             tooltipText.SetActive(false); // Hide the tooltip
         }
+    }
+
+    // Coroutine to hide the tooltip after a delay
+    private IEnumerator HideTooltipAfterDelay()
+    {
+        yield return new WaitForSeconds(tooltipDuration); // Wait for the specified time
+        tooltipText.SetActive(false); // Hide the tooltip
     }
 }
