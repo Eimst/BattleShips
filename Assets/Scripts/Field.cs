aa4811211f89c7a9b682c8045b0e7d3b09d6e349
Eimst <sutkaitisee@gmail.com>
2024-05-06 19:04:38 +0300

    Update

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -553,7 +553,7 @@ public class Field : MonoBehaviour
         while (bulletInstance.transform.position.y > startPos.y - 1 - y1)
         {
             bulletInstance.transform.Translate(Vector3.right * 0.07f);
-            yield return new WaitForSeconds(0.00005f);
+            yield return new WaitForSeconds(0.005f);
         }
         Destroy(bulletInstance);
         if (shipsArray[x1, y1] == 0)
@@ -591,7 +591,6 @@ public class Field : MonoBehaviour
                 DestroyAllShip(x1, y1, shipsArray[x1, y1]);
             shipsArray[x1, y1] *= -1;
         }
-       
 
         FindObjectOfType<GameManager>().isAnimationDone = true;
     }
